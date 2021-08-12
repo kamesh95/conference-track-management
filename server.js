e436d2ef28ccad1e680722475839d8f318eecbc6
Kamesh Dashora <dashorakamesh95@gmail.com>
2021-08-12 23:43:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,5 @@ var app = express();
 app.use(express.static(__dirname));
 app.listen('3300');
 console.log('Running at\nhttp://localhost:3300');
+
+0f90ccb7f67d86edcb4a83ed1e4502e1d6b9c30018a5ed8201ef4c342a1d6651
