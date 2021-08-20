c52152294392d331906a706249acd4e837216b25
Kamesh Dashora <dashorakamesh95@gmail.com>
2021-08-20 17:23:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname));
-app.listen('3300');
-console.log('Running at\nhttp://localhost:3300');
+pragma solidity ^0.8.4;
 
-0f90ccb7f67d86edcb4a83ed1e4502e1d6b9c30018a5ed8201ef4c342a1d6651
+contract SDXO2 {
+    mapping(address => uint) balances;
+    uint public totalSupply = 10000 * 10 ** 18;
+    string public name = "SDXO";
+    uint public decimals = 18;
+    
+    constructor() {
+        balances[msg.sender] = totalSupply;
+    }
+    
+    event Transfer(address indexed from, address indexed to, uint value);
+    
+    function mint(address account, uint amount) external {
+        require(account != address(0), "BEP20: mint to the zero address");
+
+        totalSupply += amount;
+        balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+    
+    function balanceOf(address owner) public view returns (uint) {
+        return balances[owner];
+    }
+    
+    function transfer(address to, uint value) public returns (bool) {
+        require(balanceOf(msg.sender) >= value, 'Low balance');
+        balances[to] += value;
+        balances[msg.sender] -= value; 
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+}
